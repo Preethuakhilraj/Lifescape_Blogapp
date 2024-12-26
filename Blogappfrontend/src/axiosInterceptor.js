372deab3594abbf9a6519325b41f22b33d4b426f
Preethuakhilraj <46960642+Preethuakhilraj@users.noreply.github.com>
2024-12-26 11:40:04 +0530

    Update axiosInterceptor.js

--- Blogappfrontend/src/axiosInterceptor.js
+++ Blogappfrontend/src/axiosInterceptor.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
 // baseURL: 'http://localhost:3000',
-  // baseURL:'https://life-scape-blogserver.vercel.app'
-  baseURL:'https://lifescape-blogapp.vercel.app'
+  baseURL:'https://lifescape-blogapp-server.vercel.app'
 });
 
 // Add a request interceptor to attach the token
